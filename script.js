0a63fcf4cef1d4e977c49f57e23f0bb65104e719
Raffi <raffiramadhan8488@gmail.com>
2026-06-13 03:04:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// Musik kontrol
+const musik = document.getElementById("musik");
 
+// Klik layar untuk pause/play musik
+document.body.addEventListener("click", () => {
+  if (musik.paused) {
+    musik.play();
+  } else {
+    musik.pause();
+  }
+});
+
+// Efek scroll untuk gambar
+window.addEventListener("scroll", () => {
+  const gambar = document.querySelector(".gambar img");
+  gambar.style.transform = `translateY(${window.scrollY * 0.2}px)`;
+});
